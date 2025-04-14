ee8db8218e2c6e71f617e91b33a908960fe53212
Pannawat <pchauych@usc.edu>
2025-04-13 22:07:18 -0700

    Finish modeling customers

--- method/ABM_modeling.py
+++ method/ABM_modeling.py
@@ -31,20 +31,16 @@ Customer table:
     - Income
     - Education
     - Occupation
+    
+Concerns:
+- Haven't found a way to include all categories into userpreferences
+   - Product: Category, Discount, Final Unit Price, Quantity
+   - Customer: Category, Unit Price, Quantity
+
 """
-
-walmart_customer_data_path = "/Users/macos/Personal_projects/Portfolio/Project_1_Walmart/Walmart_sim/data_source/Walmart_customer_data.csv"
-customer_segments_dist, customer_segments_col_names = dp.get_dataset_distribution(walmart_customer_data_path)
-
-
-#Customer demographic table 
-cust_demographic_col_names = ['age', 'gender', 'city_category', 'stay_in_current_city_years', 'marital_status']
-cust_purchase_behavior = ['product_category', 'purchase']
 
 
 #Customer purchasing categories frequency
-commerce_demographic_table = ['branch', 'city', 'customer_type', 'gender', 'payment_method']
-commerce_purchase_behavior = ['product_line', 'quantity', 'unit_price', 'date', 'time_of_day', 'day_name']
 transaction_purchase_behavior = ['productcategory', 'unitprice', 'quantity']
 
 
@@ -54,36 +50,55 @@ transaction_product_categories = ['productcategory', 'unitprice', 'quantity']
 commerce_product_categories = ['product_line', 'unit_price', 'quantity']
 
 
-#Customer initialization
+#Concerns
 
-    
+
+
+#Customer initialization
         
 class Cust1(Agent):
-    def __init__(self, customer_id:int, segment_id:int, 
-                 purchase_dist:gaussian_kde, 
-                 category_preference:dict,
-                 age:dict, 
-                 gender:dict, 
-                 city_category:dict, 
-                 stay_in_current_city_years:dict, 
-                 marital_status:dict):
+    
+    age:int
+    gender:str
+    city_category:str
+    stay_in_current_city_years:int
+    marital_status:str
+    purchase:gaussian_kde
+    product_category:dict
+    
+    def __init__(self, customer_id:int,
+                 segments_dist:dict,
+                 cat_dist:dict,
+                 num_dist:dict):
         """
         Initialize the customer agent based on walmart customer data.
         """
         self.customer_id = customer_id
-        self.segment_id = segment_id
+        self.segment_id = int(np.random.choice(list(segments_dist.keys()), size=1, p=list(segments_dist.values())))
         
-        self.age = age
-        self.gender = gender
-        self.city_category = city_category
-        self.stay_in_current_city_years = stay_in_current_city_years
-        self.marital_status = marital_status
-    
-        self.purchase_dist = purchase_dist
-        self.category_preference = category_preference        
-        self.purchase_history = []
-        self.visit_prob = 0.25                                  #default visit probability
+        Cat_col = ['age', 'gender', 'city_category', 'stay_in_current_city_years', 'marital_status']
+        Product_col = ['product_category']
+        Num_col = ['purchase']
+        
+        #Splitting out the product pref with other variables to create new purchases
+        cat_num = 0
+        for key, value in cat_dist[self.segment_id].items():
+            if key.lower() in Cat_col:
+                setattr(self, key.lower(), np.random.choice(list(value.keys()), size=1, p=list(value.values())))
+                cat_num += 1
+            elif key.lower() in Product_col:
+                setattr(self, key.lower(), value)
+                cat_num += 1
+            
+        num_num = 0
+        for key, value in num_dist[self.segment_id].items():
+            setattr(self, key.lower(), value)
+            num_num += 1
+        
+        assert cat_num + num_num == len(Cat_col) + len(Product_col) + len(Num_col), 'Number of customer attributes does not match expected'
+        
         self.purchase_history = defaultdict(list)
+        self.visit_prob = 0.25                                  #default visit probability
         self.budget = self._calculate_budget()    
     
     def _calculate_budget(self) -> float:
@@ -92,109 +107,136 @@ class Cust1(Agent):
         Update the probability over time.
         """
         if  len(self.purchase_history) < 5:
-            budget = self.purchase_dist.resample(1)
+            budget = self.purchase.resample(1)
         else:
             kde = gaussian_kde(self.purchase_history)
             budget = kde.resample(1)
-        return budget
+        return budget[0]
     
 
-    def make_purchase(self, item_price:dict):
+    def make_purchase(self, item_price:dict, current_date:str):
         """Determine product preference based on learned distributions."""
-        choice = np.random.choice(list(self.category_preference.keys()), size=1, p=list(self.category_preference.values()))
+        choice = np.random.choice(list(self.product_category.keys()), size=1, p=list(self.product_category.values()))
         unit_price = item_price[choice]
         quantity = np.random.normal(10, 2, 1)
         total_price = unit_price * quantity
         
         if total_price <= self.budget:
-            self.purchase_history[choice].append(total_price)
-            print(f'{self.customer_id} ought from {choice}')
-            return 1 
+            self.purchase_history[choice].append((unit_price, quantity, current_date))
         else:
             total_price = unit_price
-            print(f'Item from {choice} was bigger than budget')
-            return 0
+            self.purchase_history[choice].append((unit_price, 1, current_date))
+        return unit_price, quantity
+
     
     def step(self, item_price:dict):
         """Update customer behavior and preferences."""
         self.budget = self._calculate_budget()
         visit = 1 if random.randint(0, 100) > (self.visit_prob*100) else 0
-        if visit == 1:
-            self.make_purchase(item_price)
-        else:
-            return f'{self.customer_id} leaving empty'
-            
+        current_date = self.model.current_date.strftime('%m/%d/%Y').split('/')[1]
         
-    
+        if visit == 1:
+            self.make_purchase(item_price, current_date)
+        
 class Cust2(Agent):
-    def __init__(self, customer_id:int, segment_id:int, 
-                 purchase_dist:gaussian_kde, 
-                 category_preference:dict,
-                 branch:dict, 
-                 city:dict, 
-                 customer_type:dict, 
-                 gender:dict, 
-                 payment_method:dict,
-                 date_dist:dict):
+    
+    branch:str
+    city:str
+    customer_type:str
+    gender:str
+    payment_method:str
+    product_line:dict
+    quantity:dict | gaussian_kde
+    unit_price:gaussian_kde
+    date:dict
+    
+    def __init__(self, customer_id:int,
+                 segments_dist:dict,
+                 cat_dist:dict,
+                 num_dist:dict):
         """
         Initialize the customer agent based on e-commerce transaction data.
         New attributes compared to cust1:
             - date
         """
-        
         self.customer_id = customer_id
-        self.segment_id = segment_id
-
-        self.branch = branch
-        self.city = city
-        self.customer_type = customer_type
-        self.gender = gender
-        self.payment_method = payment_method
+        self.segment_id = int(np.random.choice(list(segments_dist.keys()), size=1, p=list(segments_dist.values())))
         
-        self.date_dist = date_dist
-        self.purchase_dist = purchase_dist
-        self.category_preference = category_preference
+        commerce_demographic_table = ['branch', 'city', 'customer_type', 'gender', 'payment_method']
+        commerce_purchase_behavior = ['product_line', 'quantity', 'unit_price', 'date']
+        
+        cat_num = 0
+        for key, value in segments_cat_dist[self.segment_id].items():
+            if key.lower() in commerce_demographic_table:
+                setattr(self, key.lower(), np.random.choice(list(value.keys()), size=1, p=list(value.values())))
+            elif key.lower() in commerce_purchase_behavior:
+                setattr(self, key.lower(), value)
+                cat_num += 1
+                
+        num_num = 0
+        for key, value in segments_num_dist[self.segment_id].items():
+            setattr(self, key.lower(), value)
+            num_num += 1
+            
+        assert cat_num + num_num == len(commerce_demographic_table) + len(commerce_purchase_behavior), 'Number of customer attributes does not match expected'
+        
         self.purchase_history = defaultdict(list)
         self.budget = self._calculate_budget()    
+        
+    def _get_quantity(self) -> float:
+        """Get quantity from either gaussian kde or categorical distribution."""
+        if isinstance(self.quantity, gaussian_kde):
+            quantity = self.quantity.resample(1)[0]
+        else:
+            quantity = float(np.random.choice(list(self.quantity.keys()), size=1, p=list(self.quantity.values())))
+        return quantity
     
     def _calculate_budget(self) -> float:
-        """Calculate initial budget based on learned distributions."""
+        """Calculate initial budget based on learned distributions for unit price and quantity."""
         if  len(self.purchase_history) < 5:
-            budget = self.purchase_dist.resample(1)
+            #Checking in case the quantity is numeric instead of categorical
+            quantity = self._get_quantity()
+            budget = self.unit_price.resample(1)[0] * quantity
         else:
             kde = gaussian_kde(self.purchase_history)
-            budget = kde.resample(1)
+            budget = kde.resample(1)[0]
             
         assert budget > 0, 'budget not created'
         
         return budget
     
-    def make_purchase(self, item_price:dict):
+    def make_purchase(self, item_price:dict) -> tuple[float, float]:
         """Determine product preference based on learned distributions."""
-        choice = np.random.choice(list(self.category_preference.keys()), size=1, p=list(self.category_preference.values()))
-        total_price = item_price[choice]
+        choice = np.random.choice(list(self.product_line.keys()), size=1, p=list(self.product_line.values()))
+        unit_price = item_price[choice]
+        quantity = self._get_quantity()
+        total_price = unit_price * quantity
+        
         if total_price <= self.budget:
-            self.purchase_history[choice].append(total_price)
-            print(f'Bought from {choice}')
-            return 1 
+            self.purchase_history[choice].append((unit_price, quantity))
         else:
-            print(f'Item from {choice} was bigger than budget')
-            return 0
+            total_price = unit_price
+            self.purchase_history[choice].append((unit_price, 1))
+        
+        assert total_price > 0, 'total price 0 or less'
+        
+        return unit_price, quantity
     
-    def get_mostcommon_date(self, time_dist:dict, top_date:int=3):
-        sorted_dist = sorted(time_dist.items(), key = lambda x: x[1], reverse=True)
-        day_list = [x[0] for x in sorted_dist[:top_date-1]]
-        return day_list
+    def get_mostcommon_date(self, top_date:int=3) -> list[str]:
+        """Pure function to get the most n common date from the date distribution."""
+        # sorted_dist = sorted(self.date.items(), key = lambda x: x[1], reverse=True)
+        # day_list = [x[0] for x in sorted_dist[0:top_date]]
+        return [x[0] for x in sorted(self.date.items(), key = lambda x: x[1], reverse=True)[0:top_date]] 
         
 
-    def step(self, date:str, time:str, item_price:dict):
+    def step(self, item_price:dict):
         """
         Update customer behavior and preferences.
         date: mm/dd/yyyy
         """
         self.budget = self._calculate_budget()
-        visit_date = self.get_mostcommon_date(self.date_dist)
-        current_date =  date.split('/')[0]
+        visit_date = self.get_mostcommon_date()
+        current_date =  self.model.current_date.strftime('%m/%d/%Y').split('/')[1]
         
         if current_date in visit_date:
             self.make_purchase(item_price)
@@ -202,6 +244,32 @@ class Cust2(Agent):
             return f'{self.customer_id} leaving empty'    
         
             
+
+def getting_segments_dist(path):
+    """
+    segment_format: {segment_name: [
+                                    probability, 
+                                    {cat_col_name: {value: probability},...}, 
+                                    {num_col_name: kde,num_col_name2: kde2,...}
+                                    ]}
+    """
+    
+    customer_segments_dist = dp.get_dataset_distribution(path)
+    segments_dist = {int(k): v[0] for k,v in customer_segments_dist.items()}
+    segments_cat_dist = {int(k): v[1] for k,v in customer_segments_dist.items()}
+    segments_num_dist = {int(k): v[2] for k,v in customer_segments_dist.items()}
+    
+    return segments_dist, segments_cat_dist, segments_num_dist
+
+# segments_dist, segments_cat_dist, segments_num_dist = getting_segments_dist("/Users/macos/Personal_projects/Portfolio/Project_1_Walmart/Walmart_sim/data_source/Walmart_cust.csv")
+# first_cust = Cust1(customer_id=1, segments_dist=segments_dist, cat_dist=segments_cat_dist, num_dist=segments_num_dist)
+
+
+segments_dist, segments_cat_dist, segments_num_dist = getting_segments_dist("/Users/macos/Personal_projects/Portfolio/Project_1_Walmart/Walmart_sim/data_source/Walmart_commerce.csv")
+print(segments_cat_dist[0])
+print(segments_num_dist[0])
+
+
 
 class Product(Agent):
     """
@@ -253,9 +321,9 @@ class Product(Agent):
         def record_sales(self, quantity):
             self.daily_sales += quantity
         
-         
         
         def step(self):
+            self.place_restock_order()
             self.fulfill_restock_orders()
             self.stock -= self.daily_sales
             if self.initial_stock < 0:
@@ -265,7 +333,7 @@ class Product(Agent):
 
             self.daily_sales = 0
     
-        
+
     
         
         
