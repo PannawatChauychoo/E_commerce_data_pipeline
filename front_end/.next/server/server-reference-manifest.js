f217e7897d8743be3c193ca4e64e4fef18dda036
Pannawat <pchauych@usc.edu>
2025-09-11 14:41:04 -0700

    Removed .next

--- front_end/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cF8h+J8/UaEBqeW1JKHIF5/akV3njSWLRS0SDq9pu58=\"\n}"
--- front_end/.next/static/chunks/app_dashboard_transactions_page_tsx_80771fb1._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7f7be971._.js",
-  "static/chunks/_e35afb30._.js"
-],
-    source: "dynamic"
-});
