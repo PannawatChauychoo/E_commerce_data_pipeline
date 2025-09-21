a112bb55512d5792009f251fd34431c7d84e2eed
Pannawat <pchauych@usc.edu>
2025-09-21 11:49:20 -0700

    Removing unnecessary dbt files

--- data_pipeline/dbt/target/compiled/dbt_walmart/models/marts/core/dim_cust1.sql
+++
@@ -1,8 +0,0 @@
-
-
-SELECT
-l.customer_id, c.segment_id, c.visit_prob,
-c.signup_date, c.last_purchase_date
-FROM "ecommerce_cloud"."dev_walmart"."stg_cust1" AS c JOIN "ecommerce_cloud"."dev_walmart"."stg_customers_lookup" AS l
-ON c.customer_id = l.external_id
-WHERE l.cust_type = 'Cust1'
--- data_pipeline/dbt/target/compiled/dbt_walmart/models/marts/core/dim_cust2.sql
+++
@@ -1,8 +0,0 @@
-
-
-SELECT
-l.customer_id, c.segment_id,
-c.signup_date, c.last_purchase_date
-FROM "ecommerce_cloud"."dev_walmart"."stg_cust2" AS c JOIN "ecommerce_cloud"."dev_walmart"."stg_customers_lookup" AS l
-ON c.customer_id = l.external_id
-WHERE l.cust_type = 'Cust2'
